cfbd5e8c7da7d619bfff9bcaa61ad2a652a14a0e
minji105 <ajhjmj9@naver.com>
2025-01-14 19:36:30 +0900

    move modal main to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,103 @@
-import React, { useState, useRef, useEffect } from "react";
-import { colornames } from "color-name-list";
-import nearestColor from 'nearest-color';
+import React, { useState } from "react";
 import { toPng } from "html-to-image";
 import download from "downloadjs";
 import styles from './Header.module.scss';
 
 export default function Header({
   bgColor,
+  compBgColor,
   font,
   handleFontChange,
   handleColorChange,
-  handleModalOpen
+  sectionRef,
+  simplifyRef
 }) {
   const fonts = ["Arial", "Times New Roman", "Courier New"];
 
+  const [previewImage, setPreviewImage] = useState({
+    full: null,
+    simplify: null,
+  });
+
+  // 이미지 저장
+  const saveScreenshot = (ref) => {
+    if (ref.current === null) return;
+
+    toPng(ref.current)
+      .then((dataUrl) => {
+        download(dataUrl, `${bgColor}_${compBgColor}.png`);
+      })
+      .catch((err) => {
+        console.error("Error capturing the screenshot:", err);
+      });
+  };
+
+  // 모달
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    const fullImage = sessionStorage.getItem("fullImage");
+    const simplifyImage = sessionStorage.getItem("simplifyImage");
+
+    setPreviewImage({
+      full: fullImage,
+      simplify: simplifyImage,
+    });
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setPreviewImage({ full: null, simplify: null });
+  };
+
   return (
-    <header className={styles.header}>
-      <input
-        type="color"
-        value={bgColor}
-        onChange={handleColorChange}
-        aria-label="Color Picker"
-      />
-      <div className={styles.fontList}>
-        {fonts.map((fontOption) => (
-          <button
-            key={fontOption}
-            onClick={() => handleFontChange(fontOption)}
-            style={{
-              fontFamily: fontOption,
-              background: font === fontOption ? '#f0f0f0' : 'none',
-            }}
-            aria-label={`Select font: ${fontOption}`}
-          >
-            Abc
-          </button>
-        ))}
-      </div>
-      <button onClick={handleModalOpen}>Save Screenshot</button>
-    </header>
+    <>
+      <header className={styles.header}>
+        <input
+          type="color"
+          value={bgColor}
+          onChange={handleColorChange}
+          aria-label="Color Picker"
+        />
+        <div className={styles.fontList}>
+          {fonts.map((fontOption) => (
+            <button
+              key={fontOption}
+              onClick={() => handleFontChange(fontOption)}
+              style={{
+                fontFamily: fontOption,
+                background: font === fontOption ? '#f0f0f0' : 'none',
+              }}
+              aria-label={`Select font: ${fontOption}`}
+            >
+              Abc
+            </button>
+          ))}
+        </div>
+        <button onClick={handleModalOpen}>Save Screenshot</button>
+      </header>
+
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <div className={styles.modalHeader}>
+              <h1>Select a image to save</h1>
+              <button className={styles.cancleBtn} onClick={handleModalClose}>cancle</button>
+            </div>
+            <div className={styles.modalImgs}>
+              <div className={styles.preview}>
+                <img src={previewImage.full} alt="Preview" className={styles.preview} />
+                <button onClick={() => saveScreenshot(sectionRef)}>save</button>
+              </div>
+              <div className={styles.preview}>
+                <img src={previewImage.simplify} alt="Preview" className={styles.preview} />
+                <button onClick={() => saveScreenshot(simplifyRef)}>save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import { colornames } from "color-name-list";
 import nearestColor from 'nearest-color';
 import { toPng } from "html-to-image";
-import download from "downloadjs";
 import styles from './Main.module.scss';
 import Header from "./Header";
 
@@ -16,10 +15,6 @@ export default function Main() {
 
   const sectionRef = useRef();
   const simplifyRef = useRef();
-  const [previewImage, setPreviewImage] = useState({
-    full: null, // full screenshot
-    simplify: null, // simplified screenshot
-  });
 
   const [font, setFont] = useState("Arial");
 
@@ -111,68 +106,18 @@ export default function Main() {
     }
   };
 
-  // 이미지 저장
-  const saveScreenshot = (ref) => {
-    if (ref.current === null) return;
-
-    toPng(ref.current)
-      .then((dataUrl) => {
-        download(dataUrl, `${bgColor}_${compBgColor}.png`);
-      })
-      .catch((err) => {
-        console.error("Error capturing the screenshot:", err);
-      });
-  };
-
-  // 모달
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleModalOpen = () => {
-    const fullImage = sessionStorage.getItem("fullImage");
-    const simplifyImage = sessionStorage.getItem("simplifyImage");
-
-    setPreviewImage({
-      full: fullImage,
-      simplify: simplifyImage,
-    });
-    setIsModalOpen(true);
-  };
-
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-    setPreviewImage({ full: null, simplify: null });
-  };
 
   return (
     <div className={styles.main}>
       <Header
         bgColor={bgColor}
+        compBgColor={compBgColor}
         font={font}
         handleFontChange={handleFontChange}
         handleColorChange={handleColorChange}
-        handleModalOpen={handleModalOpen}
+        sectionRef={sectionRef}
+        simplifyRef={simplifyRef}
       />
-
-      {isModalOpen && (
-        <div className={styles.modal}>
-          <div className={styles.modalContent}>
-            <div className={styles.modalHeader}>
-              <h1>Select a image to save</h1>
-              <button className={styles.cancleBtn} onClick={handleModalClose}>cancle</button>
-            </div>
-            <div className={styles.modalImgs}>
-              <div className={styles.preview}>
-                <img src={previewImage.full} alt="Preview" className={styles.preview} />
-                <button onClick={() => saveScreenshot(sectionRef)}>save</button>
-              </div>
-              <div className={styles.preview}>
-                <img src={previewImage.simplify} alt="Preview" className={styles.preview} />
-                <button onClick={() => saveScreenshot(simplifyRef)}>save</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
 
       <div className={styles.sections}>
         <Section
